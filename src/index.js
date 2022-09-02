823dc84443f6a2553342b4ba57a86d801470c728
Oleksandr Pavlishchev <velogitara@gmail.com>
2022-09-03 01:57:06 +0300

    add and remove contacts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from 'components/App/App';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 // import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
